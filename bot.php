d587fb208b4d429161bb8954039a74fdfee9327a
rakhim002 <124560714+rakhim002@users.noreply.github.com>
2023-02-06 11:49:00 +0000

    1

--- bot.php
+++ bot.php
@@ -6,7 +6,7 @@ require_once 'db.php';
 $db = new DB();
 
 const ADMIN_ID = 778912691; //editted  22222
-const SECONDADMIN_ID = 2100460287; //editted
+const SECONDADMIN_ID = 2100460287; //editted22222
 try {
     $bot = new \TelegramBot\Api\Client('6067817482:AAGoZ0axiOdCdPnA1epkXAW0qfeg3-SiJgw');
     $client = OpenAI::client('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');
